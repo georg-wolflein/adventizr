5864b46c63bc3ddfd92c04c466416ccc4adb00c4
Georg Wölflein <georgw7777@gmail.com>
2017-07-02 20:21:14 +0200

    Add logout functionality

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -2,3 +2,10 @@ import './body.html';
 
 import '/imports/ui/components/navbar/navbar.js'
 import '/imports/ui/components/footer/footer.js'
+
+Template.app_body.events({
+  'click .logout'(event) {
+    Meteor.logout();
+    FlowRouter.go('app.home');
+  }
+});
