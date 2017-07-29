1e4c486afc80b0d57bfc4a2914d0c3847240912a
Georg Wölflein <georgw7777@gmail.com>
2017-07-29 14:35:11 +0200

    Add calendar door resizing and moving via drag & drop

--- imports/ui/pages/calendar/edit/edit.js
+++ imports/ui/pages/calendar/edit/edit.js
@@ -1,22 +1,73 @@
-import { Calendars } from '/imports/api/calendars/calendars.js';
-import { CalendarFiles } from '/imports/api/calendar-files/calendar-files.js';
-import { Meteor } from 'meteor/meteor';
-import './edit.html';
-import './info-form/info-form.js';
-import './background-form/background-form.js';
-import './size-form/size-form.js';
+import { Calendars } from "/imports/api/calendars/calendars.js";
+import { CalendarFiles } from "/imports/api/calendar-files/calendar-files.js";
+import { Meteor } from "meteor/meteor";
+import "./edit.html";
+import "./info-form/info-form.js";
+import "./background-form/background-form.js";
+import "./size-form/size-form.js";
+
+import interact from "interactjs";
 
 Template.calendar_edit.onCreated(function() {
-  Meteor.subscribe('calendars.all');
-  Meteor.subscribe('files.calendar.all');
+  Meteor.subscribe("calendars.all");
+  Meteor.subscribe("files.calendar.all");
+});
+
+Template.calendar_edit.onRendered(function() {
+  interact(".resize-drag")
+    .draggable({
+      inertia: false,
+      onmove: onMove
+    })
+    .resizable({
+      preserveAspectRatio: false,
+      edges: { left: true, right: true, bottom: true, top: true }
+    })
+    .on('resizemove', onResizeMove);
 });
 
 Template.calendar_edit.helpers({
   calendar() {
-    var id = FlowRouter.getParam('_id');
+    var id = FlowRouter.getParam("_id");
     var calendar = Calendars.findOne(id);
     if (calendar && calendar.background)
-      calendar.backgroundImage = CalendarFiles.findOne(calendar.background).link();
+      calendar.backgroundImage = CalendarFiles.findOne(
+        calendar.background
+      ).link();
     return calendar;
   }
 });
+
+function onMove(event) {
+  var target = event.target,
+    // Keep the dragged position in the data-x/data-y attributes
+    x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // Translate the element
+  target.style.webkitTransform = target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // Update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+  console.log(event);
+}
+
+function onResizeMove(event) {
+  var target = event.target,
+    x = parseFloat(target.getAttribute("data-x")) || 0,
+    y = parseFloat(target.getAttribute("data-y")) || 0;
+
+  // Update the element's style
+  target.style.width = event.rect.width + "px";
+  target.style.height = event.rect.height + "px";
+
+  // Translate when resizing from top or left edges
+  x += event.deltaRect.left;
+  y += event.deltaRect.top;
+
+  target.style.webkitTransform = target.style.transform = "translate(" + x + "px," + y + "px)";
+
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
