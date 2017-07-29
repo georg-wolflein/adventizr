4ca072f6e7bfd9c3768ea241bd4fcf9d8b228bb3
Georg Wölflein <georgw7777@gmail.com>
2017-07-29 18:30:06 +0200

    Add visual feedback for selecting doors

--- imports/ui/pages/calendar/edit/edit.js
+++ imports/ui/pages/calendar/edit/edit.js
@@ -1,20 +1,20 @@
-import { Calendars } from "/imports/api/calendars/calendars.js";
-import { CalendarFiles } from "/imports/api/calendar-files/calendar-files.js";
-import { Meteor } from "meteor/meteor";
-import "./edit.html";
-import "./info-form/info-form.js";
-import "./background-form/background-form.js";
-import "./size-form/size-form.js";
+import { Calendars } from '/imports/api/calendars/calendars.js';
+import { CalendarFiles } from '/imports/api/calendar-files/calendar-files.js';
+import { Meteor } from 'meteor/meteor';
+import './edit.html';
+import './info-form/info-form.js';
+import './background-form/background-form.js';
+import './size-form/size-form.js';
 
-import interact from "interactjs";
+import interact from 'interactjs';
 
 Template.calendar_edit.onCreated(function() {
-  Meteor.subscribe("calendars.all");
-  Meteor.subscribe("files.calendar.all");
+  Meteor.subscribe('calendars.all');
+  Meteor.subscribe('files.calendar.all');
 });
 
 Template.calendar_edit.onRendered(function() {
-  interact(".resize-drag")
+  interact('.resize-drag')
     .draggable({
       inertia: false,
       onmove: onMove
@@ -23,12 +23,12 @@ Template.calendar_edit.onRendered(function() {
       preserveAspectRatio: false,
       edges: { left: true, right: true, bottom: true, top: true }
     })
-    .on("resizemove", onResizeMove);
+    .on('resizemove', onResizeMove);
 });
 
 Template.calendar_edit.helpers({
   calendar() {
-    var id = FlowRouter.getParam("_id");
+    var id = FlowRouter.getParam('_id');
     var calendar = Calendars.findOne(id);
     if (
       calendar &&
@@ -42,37 +42,52 @@ Template.calendar_edit.helpers({
   }
 });
 
+var selectedDoor = null;
+
+Template.calendar_edit.events({
+  'click .door'(event) {
+    var target = event.target.classList.contains('door')
+      ? event.target
+      : event.target.parentElement;
+    if (selectedDoor && selectedDoor != target.id)
+      document.getElementById(selectedDoor).style.borderStyle = 'solid';
+    selectedDoor = target.id;
+
+    target.style.borderStyle = 'dashed';
+  }
+});
+
 function onMove(event) {
   var target = event.target,
     // Keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
   // Translate the element
   target.style.webkitTransform = target.style.transform =
-    "translate(" + x + "px, " + y + "px)";
+    'translate(' + x + 'px, ' + y + 'px)';
 
   // Update the posiion attributes
-  target.setAttribute("data-x", x);
-  target.setAttribute("data-y", y);
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
 }
 
 function onResizeMove(event) {
   var target = event.target,
-    x = parseFloat(target.getAttribute("data-x")) || 0,
-    y = parseFloat(target.getAttribute("data-y")) || 0;
+    x = parseFloat(target.getAttribute('data-x')) || 0,
+    y = parseFloat(target.getAttribute('data-y')) || 0;
 
   // Update the element's style
-  target.style.width = event.rect.width + "px";
-  target.style.height = event.rect.height + "px";
+  target.style.width = event.rect.width + 'px';
+  target.style.height = event.rect.height + 'px';
 
   // Translate when resizing from top or left edges
   x += event.deltaRect.left;
   y += event.deltaRect.top;
 
   target.style.webkitTransform = target.style.transform =
-    "translate(" + x + "px," + y + "px)";
+    'translate(' + x + 'px,' + y + 'px)';
 
-  target.setAttribute("data-x", x);
-  target.setAttribute("data-y", y);
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
 }
