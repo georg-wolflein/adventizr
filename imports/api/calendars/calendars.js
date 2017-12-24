3d32f399c6f3164929d7596b2411ddf0e44360ef
Georg Wölflein <georgw7777@gmail.com>
2017-12-24 13:23:07 +0100

    Implement calendar hooks

--- imports/api/calendars/calendars.js
+++ imports/api/calendars/calendars.js
@@ -1,5 +1,17 @@
 // Definition of the calendars collection
 
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import exportCalendar from './export-calendar';
 
-export const Calendars = new Mongo.Collection('calendars');
+export const Calendars = new Mongo.Collection('calendars', {
+  transform(doc) {
+    doc.user = Meteor.users.findOne(doc.user, {
+      fields: { _id: 1, username: 1 }
+    });
+    return doc;
+  }
+});
+
+Calendars.after.insert((userId, doc) => exportCalendar(doc._id));
+Calendars.after.update((userId, doc) => exportCalendar(doc._id));
---
+++ imports/api/calendars/export-calendar.js
@@ -0,0 +1,31 @@
+import { Calendars } from '/imports/api/calendars/calendars.js';
+import fs from 'fs';
+/*
+import archiver from 'archiver';
+
+var compress = id => {
+  var output = fs.createWriteStream(
+    Meteor.settings.public.CALENDAR_FILES_PATH + '/' + id + '.zip'
+  );
+  var archive = archiver('zip', { zlib: { level: 9 } });
+  archive.pipe(output);
+  archive.directory(
+    Meteor.settings.public.CALENDAR_FILES_PATH + '/' + id,
+    false
+  );
+  archive.finalize();
+};*/
+
+module.exports = id => {
+  fs.writeFile(
+    Meteor.settings.public.CALENDAR_FILES_PATH + '/' + id + '/calendar.json',
+    JSON.stringify(Calendars.findOne(id)),
+    function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('Saved calendar.json, now exporting as zip');
+      //compress(id);
+    }
+  );
+};
